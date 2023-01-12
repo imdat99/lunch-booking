c95f5de3ff88e2b05766f99fa58b643397a0e4fa
ncquang <ncquang@cmcglobal.vn>
2023-01-12 17:24:36 +0700

    LB17-Implment-Notification

--- src/libs/api/noti/index.ts
+++ src/libs/api/noti/index.ts
@@ -1,8 +1,9 @@
 import { INoti } from '@app/server/firebaseType'
 import { NotiColection, NotiDetail } from '@app/server/useDB'
 import { store } from '@app/stores'
-import { addDoc, onSnapshot, query, updateDoc, where } from 'firebase/firestore'
-const uid = store.getState().USER.data?.uid as string
+import { addDoc, onSnapshot, query, updateDoc, where , startAfter , orderBy , limit , Query , getDocs , DocumentSnapshot, serverTimestamp} from 'firebase/firestore'
+import dayjs from 'dayjs'
+
 export const createNoti = async (data: Partial<INoti>) => {
   let isSuccess = false
   let notiId = ''
@@ -13,14 +14,60 @@ export const createNoti = async (data: Partial<INoti>) => {
   return { isSuccess, notiId }
 }
 
-export const getNoti = (fn: (r: INoti[]) => void) => {
-  onSnapshot(query(NotiColection, where('toUids', 'array-contains', uid)), (res) => {
-    fn(res.docs.map((item) => ({ ...item.data(), id: item.id, isSeen: Boolean(item.data().userSeen?.includes(uid)) })))
-  })
+export async function setUserSeen(uid:string,noti:INoti) {
+  try {
+      await updateDoc(NotiDetail(noti.id!), { userSeen: [...noti.userSeen, uid] })
+  } catch (error) {
+    console.log('ERROR SETTING USER SEEN NOTIFICATIONS IN DB', error)
+  }
 }
 
-export const seenNote = async (notiData: INoti) => {
-  if (!notiData.userSeen.includes(uid)) {
-    updateDoc(NotiDetail(notiData.id!), { userSeen: [...notiData.userSeen, uid] })
+export async function IsEventNoticed(eventId:string){
+  try {
+    var queryState = query(NotiColection, where('eventId', '==', eventId))
+    const res = await getDocs(queryState)
+    return res.docs.length > 0
+  } catch (error) {
+    console.log('ERROR GETTING NOTI OF EVENT IN DB', error)
+  }
+}
+
+
+
+export function listenCommingNoti(uid:string,eventHandler:(noti : INoti)=>void){
+  var queryState = query(NotiColection, where('toUids', 'array-contains', uid),where('date','>',dayjs(Date.now()).unix()))
+  const unsubscribe  = onSnapshot(queryState,(snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === "added") {
+        const newNoti : INoti = {...change.doc.data(),id:change.doc.id}
+        eventHandler(newNoti)
+      }
+    })})
+
+    return unsubscribe ;
+}
+
+const itemPerPage = 10
+
+export async function getListNotiByPage(uid: string,afterSnapShot : DocumentSnapshot<INoti> | null) {
+  try {
+    var queryState : Query<INoti>
+    if(afterSnapShot)
+      queryState = query(NotiColection, where('toUids', 'array-contains', uid), orderBy("date","desc"),startAfter(afterSnapShot),limit(itemPerPage));
+    else
+      queryState = query(NotiColection, where('toUids', 'array-contains', uid), orderBy("date","desc"),limit(itemPerPage));
+
+    const res = await getDocs(queryState)
+    const listNoti : INoti[] = res.docs.map((doc) => ({ ...doc.data(), id: doc.id}))
+    const isLastPage = res.docs.length < itemPerPage
+    const lastSnapShot = res.docs.length > 0 ? res.docs[res.docs.length-1] : null
+    
+    return {
+      listNoti,
+      isLastPage,
+      lastSnapShot
+    }
+  } catch (error) {
+    console.log('ERROR GETTING NOTIFICATIONS IN DB', error)
   }
 }
