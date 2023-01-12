2ec0635e8b1fd8e03d9231b2353e656e8df52b37
Dao Huong Milo <dao.huong@thoughtworks.com>
2023-01-12 11:43:53 +0700

    bug/create-bill: update title for submit button when edit

--- src/page/Events/Add.tsx
+++ src/page/Events/Add.tsx
@@ -23,7 +23,7 @@ import Snackbar from '@mui/material/Snackbar'
 import { styled } from '@mui/material/styles'
 import Tooltip from '@mui/material/Tooltip'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import * as dayjs from 'dayjs'
+import dayjs from 'dayjs'
 import _, { round } from 'lodash'
 import { useEffect, useMemo, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
@@ -76,7 +76,6 @@ function Add() {
   const listEvent = useAppSelector(listEventStore)
   const userInEvent = useMemo(() => listEventDetail.filter((event) => event.eventId === params.id), [listEventDetail, params])
   const eventInfo = useMemo(() => listEvent.find((item) => item.id === params.id), [listEvent, params.id])
-  // const { billDetail, isEditBill } = useAppSelector(billStore)
   const [eventState, setEventState] = useState<IEvent>(params.id && eventInfo ? eventInfo : initEventValue)
   const [openModalSuccess, setOpenModalSuccess] = useState<boolean>(false)
   const [listBillOwner, setListBillOwner] = useState<User[]>([])
@@ -85,6 +84,7 @@ function Add() {
   const [bonusType, setBonusType] = useState<bonusTypeEnum>(bonusTypeEnum.PERCENT)
   const navigate = useNavigate()
   // const dispatch = useAppDispatch()
+  const isEdit = useMemo(() => !!params.id && !!eventInfo, [eventInfo, params.id])
 
   useEffect(() => {
     setListBillOwner(sortListByPaidCount([...selectedListMember]))
@@ -251,6 +251,7 @@ function Add() {
   useEffect(() => {
     setListBillOwner([...selectedListMember])
   }, [selectedListMember])
+
   useEffect(() => {
     const bonus = calBonus(eventState.billAmount || 0, eventState.tip || 0)
     const total = (eventState.billAmount || 0) + bonus
@@ -489,7 +490,7 @@ function Add() {
           </Box>
           <Box className="flex justify-center my-7">
             <ButtonStyled variant="contained" onClick={handleCreateEvent} disabled={!eventState.eventName}>
-              <Typography>Tạo hóa đơn</Typography>
+              <Typography>{isEdit ? 'Cập nhật' : 'Tạo hóa đơn'}</Typography>
             </ButtonStyled>
           </Box>
         </CardContent>
