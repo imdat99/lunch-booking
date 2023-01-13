b99d1d2459a5c3e448268d1bc35c6e3ecd4abcb3
dat09 <ltdat8@cmcglobal.vn>
2023-01-13 10:40:14 +0700

    fix amount list 13/01

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { defineConfig } from 'vite'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // eslint({
+    //   lintOnStart: true,
+    // }),
+  ],
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, './src'),
