421fb1e5df6b1f1a98366124dab177d73bf561d1
Dao Huong Milo <dao.huong@thoughtworks.com>
2023-01-16 17:39:59 +0700

    hotfix/dev: change text status of paid or not paid

--- src/libs/constant/text.constant.ts
+++ src/libs/constant/text.constant.ts
@@ -4,5 +4,3 @@ export const TEXT__PAYMENT_REMIND = 'Nhắc nhở trả tiền' as const
 export const TEXT__PAYMENT_REMIND_MSG = 'Trả tiền cho tui đi!' as const
 export const TEXT__PAYMENT_PAID = 'Thông báo đã trả' as const
 export const TEXT__PAYMENT_PAID_MSG = 'Tui trả rồi nhé!' as const
-export const TEXT__PAID = 'Đã trả' as const
-export const TEXT__UNPAID = 'Chưa trả' as const
