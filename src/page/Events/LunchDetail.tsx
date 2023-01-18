f394b08579161cfd8e7da2ef5994847ec07a860f
Duy. Bui Quang - CMC Global DE1 <bqduy1@cmcglobal.vn>
2023-01-18 10:10:16 +0700

    LB32 fix bug UI

--- src/page/Events/LunchDetail.tsx
+++ src/page/Events/LunchDetail.tsx
@@ -11,7 +11,7 @@ import { userStore } from '@app/stores/user'
 import BorderColorIcon from '@mui/icons-material/BorderColor'
 import DeleteIcon from '@mui/icons-material/Delete'
 import ReplyIcon from '@mui/icons-material/Reply'
-import { Typography } from '@mui/material'
+import { Container, Typography } from '@mui/material'
 import Alert from '@mui/material/Alert'
 import Button from '@mui/material/Button'
 import Dialog from '@mui/material/Dialog'
@@ -24,7 +24,6 @@ import dayjs from 'dayjs'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { bonusTypeEnum } from './Add'
-
 
 const LunchDetail = () => {
   // navigate
@@ -147,197 +146,202 @@ const LunchDetail = () => {
           <span className="font-bold">{openAlert}</span>
         </Alert>
       </Snackbar>
-      <div className="bg-gradient-to-t from-green-300 to-light-color rounded-b-3xl">
-        <div className="flex justify-between p-3">
-          <button
-            className="h-[36px]"
-            onClick={() => {
-              history.back()
-            }}
-          >
-            <ReplyIcon fontSize={'large'} />
-          </button>
-          <div className="flex flex-col text-center">
-            <div className={'mx-auto relative mb-5 rounded-full border-4 p-1 ' + (isHost ? 'border-red-500' : 'border-green-500')}>
-              <img
-                src={hostInfo?.photoURL || 'https://picsum.photos/200/300?grayscale'}
-                referrerPolicy="no-referrer"
-                className="w-24 h-24 rounded-full"
-                alt=""
-              />
-              <span
-                className={
-                  'absolute py-1 px-2 block font-normal text-white rounded-lg -bottom-5 inset-x-2/4 -translate-x-2/4 ' +
-                  (isHost ? 'bg-red-600 w-[70px]' : 'bg-green-600 w-[80px]')
-                }
-              >
-                {isHost ? TEXT__HOST : TEXT__MEMBER}
-              </span>
-            </div>
-            <h2 className="text-2xl text-center mb-2">{eventInfo?.eventName}</h2>
-            <time className="mb-2">{eventInfo?.date}</time>
-            <div className="my-4 flex-wrap">
-              <div className="relative overflow-x-auto">
-                <table className="w-full text-left">
-                  <tbody>
-                    <tr>
-                      <th scope="row" className="font-normal pr-4">
-                        {TEXT__HOST}
-                      </th>
-                      <td>
-                        <b>{eventInfo?.userPayName || 'Chưa chọn chủ trì'}</b>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th scope="row" className="font-normal pr-4">
-                        Tham gia
-                      </th>
-                      <td>
-                        <b>{userInEvent?.length} người</b>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-          <div>
-            {isHost || !hostInfo ? (
-              <button className="h-[36px]" onClick={() => navigate(`/events/edit/${params.id}`)}>
-                <BorderColorIcon fontSize={'large'} />
-              </button>
-            ) : (
-              <div className="h-[36px] w-[36px]"></div>
-            )}
-          </div>
-        </div>
-      </div>
-      <div className="py-3 px-5">
-        <div className="flex justify-between">
-          <span className="text-gray-400 font-bold block mb-3">Tổng bill</span>
-          <p className="text-end">
-            <span className="text-black">{formatMoney(eventInfo?.billAmount)}</span>
-          </p>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-400 font-bold block mb-3">Hoa hồng</span>
-          <p className="text-end">
-            <span className="text-black">{eventInfo?.bonusType === bonusTypeEnum.MONEY ? formatMoney(eventInfo?.tip) : eventInfo?.tip + '%'}</span>
-          </p>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-400 font-bold block mb-3">Tổng tiền</span>
-          <p className="text-end">
-            <span className="text-black">{formatMoney(eventInfo?.totalAmount)}</span>
-          </p>
-        </div>
-        <div className="border-y-[1px] border-gray-400">
-          <div className="relative overflow-x-auto">
-            <table className="w-full text-left">
-              <thead>
-                <tr className="text-gray-400 font-bold">
-                  <th scope="col" className="py-3">
-                    Thành viên
-                  </th>
-                  <th scope="col" className="py-3 text-center">
-                    Bill
-                  </th>
-                  <th scope="col" className="py-3 text-right">
-                    Pay
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {userInEvent.map((user) => (
-                  <tr key={user.uid}>
-                    <td>
-                      <label className="inline-flex items-center cursor-pointer">
-                        <input
-                          type="checkbox"
-                          className="w-6 h-6 accent-green-600 text-green-600 border-0 rounded-md focus:ring-0"
-                          readOnly
-                          disabled={!user.isPaid}
-                          checked={user.isPaid}
-                        />
-                        <span className="ml-3">{user.name || user.email}</span>
-                      </label>
-                    </td>
-                    <td className="text-center">{formatMoney(user.amount, false)}</td>
-                    <td className="text-right">{formatMoney(user.amountToPay, false)}</td>
-                  </tr>
-                ))}
-              </tbody>
-              <tfoot>
-                <tr className="text-right text-gray-500">
-                  <td colSpan={3}>
-                    <em className="text-sm">
-                      * Đơn vị tính <b>K VNĐ</b>
-                    </em>
-                  </td>
-                </tr>
-              </tfoot>
-            </table>
-          </div>
-        </div>
-        {!isPaid ? (
-          <>
-            <div className="my-3">
-              <span className="text-gray-400 font-bold block mb-3">Bank Account</span>
-              <p>
-                Chủ tài khoản: {hostInfo?.bankAccountName} <br />
-                Ngân hàng: {hostInfo?.bankName} <br />
-                Số Tài khoản: <b>{hostInfo?.bankAccount}</b>{' '}
-                <button className="px-2 rounded bg-gray-300 mb-3" onClick={handleClick}>
-                  Copy
-                </button>
+
+      <div className="bg-gradient-to-t from-green-300 to-light-color rounded-b-3xl w-19">
+        <Container>
+          <div className="flex justify-between p-3">
+            <button
+              className="h-[36px]"
+              onClick={() => {
+                history.back()
+              }}
+            >
+              <ReplyIcon fontSize={'large'} />
+            </button>
+            <div className="flex flex-col text-center">
+              <div className={'mx-auto relative mb-5 rounded-full border-4 p-1 ' + (isHost ? 'border-red-500' : 'border-green-500')}>
                 <img
-                  className="w-96 h-auto mx-auto"
-                  src={hostInfo?.qrCodeURL || ''}
+                  src={hostInfo?.photoURL || 'https://picsum.photos/200/300?grayscale'}
                   referrerPolicy="no-referrer"
-                  alt={hostInfo?.bankName || '' + ' - ' + hostInfo?.bankAccount || ''}
+                  className="w-24 h-24 rounded-full"
+                  alt=""
                 />
-              </p>
-            </div>
-            <div className="my-3">
-              <span className="text-gray-400 font-bold block mb-3">Action</span>
-              <div className="flex w-full">
-                <button
-                  type="button"
-                  onClick={handleNoti}
-                  disabled={disableNoti}
+                <span
                   className={
-                    'focus:outline-none text-white focus:ring-4 font-medium rounded-lg px-5 py-2.5 mx-auto ' +
-                    (isHost ? 'bg-green-600 hover:bg-green-700 focus:ring-green-400 ' : 'bg-[#B91D37] ') +
-                    (disableNoti ? 'cursor-not-allowed hover:bg-green-600' : '')
+                    'absolute py-1 px-2 block font-normal text-white rounded-lg -bottom-5 inset-x-2/4 -translate-x-2/4 ' +
+                    (isHost ? 'bg-red-600 w-[70px]' : 'bg-green-600 w-[80px]')
                   }
                 >
-                  {isHost ? TEXT__PAYMENT_REMIND : TEXT__PAYMENT_PAID}
-                </button>
+                  {isHost ? TEXT__HOST : TEXT__MEMBER}
+                </span>
+              </div>
+              <h2 className="text-2xl text-center mb-2">{eventInfo?.eventName}</h2>
+              <time className="mb-2">{eventInfo?.date}</time>
+              <div className="my-4 flex-wrap">
+                <div className="relative overflow-x-auto">
+                  <table className="w-full text-left">
+                    <tbody>
+                      <tr>
+                        <th scope="row" className="font-normal pr-4">
+                          {TEXT__HOST}
+                        </th>
+                        <td>
+                          <b>{eventInfo?.userPayName || 'Chưa chọn chủ trì'}</b>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row" className="font-normal pr-4">
+                          Tham gia
+                        </th>
+                        <td>
+                          <b>{userInEvent?.length} người</b>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
-            {(isHost || !hostInfo) && (
+            <div>
+              {isHost || !hostInfo ? (
+                <button className="h-[36px]" onClick={() => navigate(`/events/edit/${params.id}`)}>
+                  <BorderColorIcon fontSize={'large'} />
+                </button>
+              ) : (
+                <div className="h-[36px] w-[36px]"></div>
+              )}
+            </div>
+          </div>
+        </Container>
+      </div>
+      <Container>
+        <div className="py-3 px-5">
+          <div className="flex justify-between">
+            <span className="text-gray-400 font-bold block mb-3">Tổng bill</span>
+            <p className="text-end">
+              <span className="text-black">{formatMoney(eventInfo?.billAmount)}</span>
+            </p>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-gray-400 font-bold block mb-3">Hoa hồng</span>
+            <p className="text-end">
+              <span className="text-black">{eventInfo?.bonusType === bonusTypeEnum.MONEY ? formatMoney(eventInfo?.tip) : eventInfo?.tip + '%'}</span>
+            </p>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-gray-400 font-bold block mb-3">Tổng tiền</span>
+            <p className="text-end">
+              <span className="text-black">{formatMoney(eventInfo?.totalAmount)}</span>
+            </p>
+          </div>
+          <div className="border-y-[1px] border-gray-400">
+            <div className="relative overflow-x-auto">
+              <table className="w-full text-left">
+                <thead>
+                  <tr className="text-gray-400 font-bold">
+                    <th scope="col" className="py-3">
+                      Thành viên
+                    </th>
+                    <th scope="col" className="py-3 text-center">
+                      Bill
+                    </th>
+                    <th scope="col" className="py-3 text-right">
+                      Pay
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {userInEvent.map((user) => (
+                    <tr key={user.uid}>
+                      <td>
+                        <label className="inline-flex items-center cursor-pointer">
+                          <input
+                            type="checkbox"
+                            className="w-6 h-6 accent-green-600 text-green-600 border-0 rounded-md focus:ring-0"
+                            readOnly
+                            disabled={!user.isPaid}
+                            checked={user.isPaid}
+                          />
+                          <span className="ml-3">{user.name || user.email}</span>
+                        </label>
+                      </td>
+                      <td className="text-center">{formatMoney(user.amount, false)}</td>
+                      <td className="text-right">{formatMoney(user.amountToPay, false)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+                <tfoot>
+                  <tr className="text-right text-gray-500">
+                    <td colSpan={3}>
+                      <em className="text-sm">
+                        * Đơn vị tính <b>K VNĐ</b>
+                      </em>
+                    </td>
+                  </tr>
+                </tfoot>
+              </table>
+            </div>
+          </div>
+          {!isPaid ? (
+            <>
               <div className="my-3">
-                <span className="text-gray-400 font-bold block mb-3">Danger Zone</span>
+                <span className="text-gray-400 font-bold block mb-3">Bank Account</span>
+                <p>
+                  Chủ tài khoản: {hostInfo?.bankAccountName} <br />
+                  Ngân hàng: {hostInfo?.bankName} <br />
+                  Số Tài khoản: <b>{hostInfo?.bankAccount}</b>{' '}
+                  <button className="px-2 rounded bg-gray-300 mb-3" onClick={handleClick}>
+                    Copy
+                  </button>
+                  <img
+                    className="w-96 h-auto mx-auto"
+                    src={hostInfo?.qrCodeURL || ''}
+                    referrerPolicy="no-referrer"
+                    alt={hostInfo?.bankName || '' + ' - ' + hostInfo?.bankAccount || ''}
+                  />
+                </p>
+              </div>
+              <div className="my-3">
+                <span className="text-gray-400 font-bold block mb-3">Action</span>
                 <div className="flex w-full">
                   <button
                     type="button"
-                    onClick={() => {
-                      setConfirmDialog(true)
-                    }}
-                    className={'focus:outline-none text-white font-medium rounded-lg px-5 py-2.5 mx-auto bg-red-600 hover:bg-red-700 focus:ring-red-400'}
+                    onClick={handleNoti}
+                    disabled={disableNoti}
+                    className={
+                      'focus:outline-none text-white focus:ring-4 font-medium rounded-lg px-5 py-2.5 mx-auto ' +
+                      (isHost ? 'bg-green-600 hover:bg-green-700 focus:ring-green-400 ' : 'bg-[#B91D37] ') +
+                      (disableNoti ? 'cursor-not-allowed hover:bg-green-600' : '')
+                    }
                   >
-                    Xoá Bill
+                    {isHost ? TEXT__PAYMENT_REMIND : TEXT__PAYMENT_PAID}
                   </button>
                 </div>
               </div>
-            )}
-          </>
-        ) : (
-          <div className="my-3">
-            <img className="w-96 h-auto mx-auto" src="/paid.png" alt="aaa" />
-          </div>
-        )}
-      </div>
+              {(isHost || !hostInfo) && (
+                <div className="my-3">
+                  <span className="text-gray-400 font-bold block mb-3">Danger Zone</span>
+                  <div className="flex w-full">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        setConfirmDialog(true)
+                      }}
+                      className={'focus:outline-none text-white font-medium rounded-lg px-5 py-2.5 mx-auto bg-red-600 hover:bg-red-700 focus:ring-red-400'}
+                    >
+                      Xoá Bill
+                    </button>
+                  </div>
+                </div>
+              )}
+            </>
+          ) : (
+            <div className="my-3">
+              <img className="w-96 h-auto mx-auto" src="/paid.png" alt="aaa" />
+            </div>
+          )}
+        </div>
+      </Container>
     </div>
   )
 }
--- src/page/Notification/index.tsx
+++ src/page/Notification/index.tsx
@@ -1,14 +1,16 @@
-import NotificationCard from './NotificationCard'
-import { useEffect, useState, ReactNode } from 'react'
-import { useAppSelector, useAppDispatch } from '@app/stores/hook'
-import { listNotiSelector, initializeNotiList, isLastPageSelector, updateNoti, setUserReadNoti , updateNewNotiCount} from '@app/stores/noti'
-import Typography from '@mui/material/Typography';
-import { userStore } from '@app/stores/user'
-import { getUserByUid } from '@app/libs/api/userAPI'
 import { updateLastTimeCheckNoti } from '@app/libs/api/noti'
-import InfinitScroll from 'react-infinite-scroll-component'
-import dayjs from 'dayjs'
+import { getUserByUid } from '@app/libs/api/userAPI'
 import { FORMAT__DATE } from '@app/libs/constant'
+import { useAppDispatch, useAppSelector } from '@app/stores/hook'
+import { initializeNotiList, isLastPageSelector, listNotiSelector, setUserReadNoti, updateNewNotiCount, updateNoti } from '@app/stores/noti'
+import { userStore } from '@app/stores/user'
+import { Container } from '@mui/material'
+import Typography from '@mui/material/Typography'
+import dayjs from 'dayjs'
+import { ReactNode, useEffect, useState } from 'react'
+import InfinitScroll from 'react-infinite-scroll-component'
+
+import NotificationCard from './NotificationCard'
 
 export default function Notification() {
   const listNoti = useAppSelector(listNotiSelector)
@@ -20,8 +22,8 @@ export default function Notification() {
   useEffect(() => {
     upDateCheckTime()
     dispatch(updateNewNotiCount(userInfo.uid!))
-    async function upDateCheckTime(){
-    await  updateLastTimeCheckNoti(userInfo.uid!)
+    async function upDateCheckTime() {
+      await updateLastTimeCheckNoti(userInfo.uid!)
     }
   }, [userInfo])
 
@@ -31,22 +33,22 @@ export default function Notification() {
 
       const listCard = listNoti.map((noti, index) => {
         return (
-            <div className="mb-[0.625rem]" key={noti.id}>
-              <NotificationCard
-                link={`/events/${noti.eventId}`}
-                time={`${dayjs(noti.date * 1000).format(FORMAT__DATE)}`}
-                content={
-                  <p className='text-black text-[0.875rem] leading-[1.125rem]'>
-                    Từ <b>{listUser[index]?.name}</b> : {noti.content}
-                  </p>
-                }
-                isRead={noti.userSeen.includes(userInfo?.uid!)}
-                avatarSrc={listUser[index]?.photoURL!}
-                onClick={() => {
-                  dispatch(setUserReadNoti(userInfo?.uid!, noti))
-                }}
-              />
-            </div>
+          <div className="mb-[0.625rem] rounded-3xl block" key={noti.id}>
+            <NotificationCard
+              link={`/events/${noti.eventId}`}
+              time={`${dayjs(noti.date * 1000).format(FORMAT__DATE)}`}
+              content={
+                <p className="text-black text-[0.875rem] leading-[1.125rem]">
+                  Từ <b>{listUser[index]?.name}</b> : {noti.content}
+                </p>
+              }
+              isRead={noti.userSeen.includes(userInfo?.uid!)}
+              avatarSrc={listUser[index]?.photoURL!}
+              onClick={() => {
+                dispatch(setUserReadNoti(userInfo?.uid!, noti))
+              }}
+            />
+          </div>
         )
       })
       setListCard(listCard)
@@ -59,17 +61,17 @@ export default function Notification() {
   }
 
   return (
-    <>
+    <Container>
       <div className="flex flex-col justify-start">
-        <div className="mt-[1.875rem] mb-[1.875rem] h-[5.625rem]">
-          <Typography  sx={{fontSize:"1.5rem", lineHeight:"1.875rem",textAlign:"center" }}>Thông báo</Typography>
+        <div className="mt-[1.875rem] mb-[1.875rem]">
+          <Typography sx={{ fontSize: '1.5rem', lineHeight: '1.875rem', textAlign: 'center' }}>Thông báo</Typography>
         </div>
-        <div className="flex flex-col content-center overflow-y-auto ">
+        <div className="flex flex-col content-center overflow-y-auto mx-auto w-11/12 max-w-md">
           <InfinitScroll hasMore={!isLastPage} next={updateNotiList} loader={<p>Loading...</p>} dataLength={listCard.length}>
             {listCard}
           </InfinitScroll>
         </div>
       </div>
-    </>
+    </Container>
   )
 }
