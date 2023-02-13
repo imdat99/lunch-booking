07cbe7386b49a267170869761308838b32f6082e
Phúc (Scott) Nguyễn <en.deathraven@gmail.com>
2023-02-13 14:31:42 +0700

    Add Vite PWA library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
-import eslint from 'vite-plugin-eslint'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +10,25 @@ export default defineConfig({
     // eslint({
     //   lintOnStart: true,
     // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png'],
+      manifest: {
+        name: 'Happy Lunch',
+        short_name: 'Happy Lunch',
+        description: 'Booking and Share Lunch Money',
+        background_color: '#FFF',
+        theme_color: '#FFF',
+        start_url: '/',
+        icons: [
+          {
+            src: 'favicon.png',
+            size: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
