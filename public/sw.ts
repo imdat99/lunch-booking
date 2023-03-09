2ec5bab50692befdfcf3e2c3f7f042275ca07a6c
Duy. Bui Quang - CMC Global DE1 <bqduy1@cmcglobal.vn>
2023-03-09 11:19:41 +0700

    update sw.js

--- public/sw.ts
+++ public/sw.ts
@@ -1,3 +1,22 @@
+// This is the "Offline copy of pages" service worker
+
+const CACHE = 'pwabuilder-offline'
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
+  }
+})
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE,
+  })
+)
+
 //browser push notification "onClick" event heandler
 self.addEventListener('notificationclick', function (event) {
   console.log('[Service Worker] Notification click Received.')
